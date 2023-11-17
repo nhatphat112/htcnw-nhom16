c9ba7394eb1cf3c20e577b670c68c344a7a843bc
vanthanh86w <vanthanh86w@gmail.com>
2023-11-17 11:43:42 +0700

    Update master data (product - addcart-css)

---
+++ assets/js/cart.js
@@ -0,0 +1,43 @@
+let soluong = localStorage.getItem("totalqty");
+let arrCart = JSON.parse(localStorage.getItem("myCart"));
+console.log(arrCart);
+document.querySelector(".cartno").innerText = soluong;
+let strHeader = `   <tr>
+                        <th>Name</th>
+                        <th>Picture</th>
+                        <th>Unit Price</th>
+                        <th>Quantity</th>
+                        <th>Sub Total</th>
+                        <th><th>
+                    </tr>`;
+let strdata = "";
+total = 0;
+arrCart.forEach(item => {
+    strdata += `<tr>
+                    <td>
+                        ${item.sp.tensp}
+                    </td>
+                    <td>
+                        <img src="${item.sp.hinhsp}" alt="" width="50px" height="50px">
+                    </td>
+                    <td>${item.sp.price}$</td>
+                    <td>
+                        <button>+</button>
+                        <input type="number" value="${item.qty}" name="txtqty" id="txtqty" style="width: 40px; text-align: center;">
+                        <button>-</button>
+                    </td>
+                    <td>${item.sp.price * item.qty}$</td>
+                    <td>
+                        <button id="delete" onclick="deletecart()">Delete</button>
+                    </td>
+                </tr>`
+    total += item.sp.price * item.qty
+});
+let strfooter = `   <tr>
+                        <td colspan="4" style="text-align: center">Total</td>
+                        <td colspan="2" id="Total">
+
+                        </td>
+                    </tr>`
+document.querySelector("#tbcart").innerHTML = strHeader + strdata + strfooter;
+document.querySelector("#Total").innerHTML = total + "$";
--- assets/js/product.js
+++ assets/js/product.js
@@ -43,9 +43,126 @@
 // 		explore.append(div3Element)
 // 	})
 // }
+
+//shopping
+// let sp = {masp:"SP0001", tensp:"Keyboard DareU EK87 490", hinhsp:"assets/images/explore/e2-Pioneer-DJ-DM-50D-W-White-7630.jpg", price:"400"}
+
+const arrSP = [
+    {masp:"SP0001",typesp:"keyboard", tensp:"Keyboard DareU EK87 490", hinhsp:"assets/images/explore/key-board-DareU-EK87-490.jpeg", price:"47"},
+    {masp:"SP0002",typesp:"loudspeaker", tensp:"Pioneer DJ DM 50D W-White", hinhsp:"assets/images/explore/e2-Pioneer-DJ-DM-50D-W-White-7630.jpg", price:"400"},
+    {masp:"SP0003",typesp:"mic", tensp:"IMMERSE GV60 STREAMING MIC", hinhsp:"assets/images/explore/e3-IMMERSE-GV60-STREAMING-MIC.png", price:"600"},
+    {masp:"SP0004",typesp:"mouse", tensp:"Pauroty Gaming Mouse RGB Wired Ergonomic", hinhsp:"assets/images/explore/e4-Pauroty-Gaming-Mouse-RGB-Wired-Ergonomic.jpg", price:"25"},
+    {masp:"SP0005",typesp:"loudspeaker", tensp:"KRK Rokit 7 g4", hinhsp:"assets/images/explore/krk.png", price:"700"},
+    {masp:"SP0006",typesp:"loudspeaker", tensp:"Edfifier W820NB White Bluetooth", hinhsp:"./assets/images/explore/e6-Edfifier-W820NB-White-Bluetooth.jpg", price:"1k"},
+
+];
+
+var str = "";
+for (let i = 0; i < arrSP.length; i++){
+    var obj = JSON.stringify(arrSP[i]);
+    str += `
+    <div class="col-md-4 col-sm-6" id="product" type="${arrSP[i].typesp}">
+        <div class="single-explore-item">
+            <div class="single-explore-img">
+                <img src="${arrSP[i].hinhsp}" type="${arrSP[i].typesp}" height="25" alt="explore image">
+                <div class="single-explore-img-info">
+                    <button onclick="window.location.href='#'">best rated</button>
+                    <div class="single-explore-image-icon-box">
+                        <ul>
+                            <li>
+                                <div class="single-explore-image-icon">
+                                    <i class="fa fa-arrows-alt"></i>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="single-explore-image-icon">
+                                    <i class="fa fa-bookmark-o"></i>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="single-explore-txt bg-theme-1">
+                <h2><a href="#">${arrSP[i].tensp}</a></h2>
+                <p class="explore-rating-price">
+                    <span class="explore-price-box">
+                        Price
+                        <span class="explore-price">${arrSP[i].price}$</span>
+                    </span>
+                        <a href="#">Keyboard</a>
+                </p>
+                <div class="explore-open-close-part">
+                    <div class="row">
+                        <div class="col-sm-9">
+                            <button onclick=\'addCart(\`${obj}\`)\'>Add Cart</button>
+                            <button onclick=\'showdetail(\`${obj}\`)\'>Detail</button>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="explore-map-icon">
+                                <a href="#"><i data-feather="heart"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+}
+document.querySelector('.explore-content').innerHTML = str;
+
+let soluong = localStorage.getItem("totalqty");
+if(soluong == null){
+    soluong = 0;
+}
+document.querySelector(".cartno").innerText = soluong;
+let arrCart = localStorage.getItem("myCart");
+if(arrCart == null){
+    arrCart = [];
+}else{
+    arrCart = JSON.parse(localStorage.getItem("myCart"));
+}
+console.log(arrCart);
+
+function addCart(objSP){
+    console.log(objSP);
+    objSP = JSON.parse(objSP);
+    var item;
+    var qtyitem = 0;
+    var flag = false;
+    console.log("current"+ objSP.masp);
+    for (let i = 0; i < arrCart.length; i++){
+        if(arrCart[i].sp.masp == objSP.masp){
+            console.log("spi"+ arrCart[i].sp.masp);
+            arrCart[i].qty ++;
+            var flag = true;
+            break;
+        }
+    }
+    if(flag == false){
+        let cartitem = {sp: objSP, qty:1};
+        arrCart.push(cartitem);
+    }
+
+    soluong++;
+    document.querySelector(".cartno").innerText = soluong;
+    localStorage.setItem("totalqty", soluong + "");
+    localStorage.setItem("myCart", JSON.stringify(arrCart))
+}
+
+function showdetail(objSP){
+    console.log(objSP);
+    window.localStorage.setItem("sanpham", objSP);
+    location.href = "detail.html";
+}
+
+
+
+//Lọc sản phẩm
 var imgList = document.querySelectorAll('#product')
 var explore = document.querySelector('.explore-content')
-var btnList = document.querySelectorAll('button')
+var btnList = document.querySelectorAll('#button_cat')
 function activeButton(btnActive){
     btnList.forEach(btn=>{
         btn.classList.remove('active')
@@ -67,3 +184,30 @@ btnList.forEach(btn=>{
         })
     })
 })
+
+//Detail
+
+// var imgList = document.querySelectorAll('#product')
+// var explore = document.querySelector('.explore-content')
+// var btnList = document.querySelectorAll('button')
+// function activeButton(btnActive){
+//     btnList.forEach(btn=>{
+//         btn.classList.remove('active')
+//     })
+//     btnActive.classList.add('active')
+// }
+
+// btnList.forEach(btn=>{
+//     btn.addEventListener('click', e=>{
+//         activeButton(e.currentTarget)
+//         let type = e.currentTarget.getAttribute('type')
+//         imgList.forEach(img=>{
+//             let foodType = img.getAttribute('type')
+//             if(type == 'all' || foodType == type){
+//                 img.classList.remove('hide')
+//             }else{
+//                 img.classList.add('hide')
+//             }
+//         })
+//     })
+// })
